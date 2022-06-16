5f393da348f53013691c420ca86d9d1e596b2e94
mayank yadav <mayank007deshwal@gmail.com>
2022-06-16 15:57:14 +0530

    trying to deploy

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
+
+validates :name, :ticker, presence: true
 	def self.new_lookup(ticker_symbol)
 		client = IEX::Api::Client.new(
                            publishable_token: 'Tpk_3b3894602abc49608cba6e0ff1687e68',                                                 secret_token: 'sk_1fd20e0ad3f64036829da0448052baae',
@@ -10,5 +14,9 @@ class Stock < ApplicationRecord
     rescue => exception
         return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+     where(ticker: ticker_symbol).first
   end
 end
